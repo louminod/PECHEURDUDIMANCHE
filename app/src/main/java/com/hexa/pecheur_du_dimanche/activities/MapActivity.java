1c4741ac6e94831929fa882b11d6550036a85b94
louis-marie <louismarie.merminod@gmail.com>
2021-03-21 14:45:21 +0100

    MapActivity comments

--- app/src/main/java/com/hexa/pecheur_du_dimanche/activities/MapActivity.java
+++ app/src/main/java/com/hexa/pecheur_du_dimanche/activities/MapActivity.java
@@ -51,75 +51,100 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * This activity is responsible of display a map with the user position and all the known stations.
+ */
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 5445;
+    // Google map service
     private GoogleMap googleMap;
+
+    // First time opening the map
+    private boolean firstTimeFlag = true;
+
+    // The current user position (converted as an address)
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 5445;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private Location currentLocation;
-    private boolean firstTimeFlag = true;
     private Adresse currentAddress;
+
+    // The application context
     private Context context;
 
+    // Static attributes allowing access from everywhere
     public static List<Station> stations;
     public static HashMap<String, String> markerMap;
     public static boolean loadDone;
 
+    /**
+     * This callback is triggered by a location change. It will reload the current location and the stations.
+     */
     private final LocationCallback mLocationCallback = new LocationCallback() {
         @Override
         public void onLocationResult(LocationResult locationResult) {
+            // Get the current location and convert it to an address
             if (currentAddress == null) {
                 super.onLocationResult(locationResult);
                 locationResult.getLastLocation();
                 currentLocation = locationResult.getLastLocation();
+
+                // Use an API to reverse the location to an address
                 currentAddress = APIAdresse.reverseLocation(currentLocation);
+
+                // If it is the first map opening, display the current location with an animation
                 if (firstTimeFlag && googleMap != null) {
                     animateCamera(currentLocation);
                     firstTimeFlag = false;
                 }
+
+                // Display a marker on the map
                 showLocationMarker(currentLocation.getLatitude(), currentLocation.getLongitude());
             }
 
+            // If there is a currant address and the stations are not loaded yet
             if (currentAddress != null && !MapActivity.loadDone) {
-                // MapActivity.stations = WaterTempApi.stationsForDepartment(currentAddress.getPostcode().substring(0, 2));
+
+                // Create a thread responsible of loading the stations
                 new Thread(() -> {
+                    // Load the stations from the API
                     runOnUiThread(() -> Toast.makeText(context, "Chargement des stations...", Toast.LENGTH_LONG).show());
                     MapActivity.stations = WaterTempApi.stations();
                     runOnUiThread(() -> Toast.makeText(context, "Chargement terminé", Toast.LENGTH_SHORT).show());
 
-                    String lastStationToLoad = MapActivity.stations.get(MapActivity.stations.size() - 1).getCodeStation();
-
+                    // Show each station on the map
                     MapActivity.stations.forEach(station -> {
                         if (!station.isLoaded()) {
-                            new Thread(() -> {
-                                // WaterTempApi.fetchStationChronique(station);
-                                // WaterQualityApi.fetchStationEnvironmentalCondition(station);
-                                station.setLoaded(true);
-                                if (station.getCodeStation().equals(lastStationToLoad)) {
-                                    MapActivity.loadDone = true;
-                                }
-                            }).start();
+                            station.setLoaded(true);
                             runOnUiThread(() -> showStationMarker(station));
                         }
                     });
+
+                    // Set the loading state to done
+                    MapActivity.loadDone = true;
                 }).start();
             }
         }
     };
 
+    // Show the user location marker
     private void showLocationMarker(double latitude, double longitude) {
+        // Set a specific icon for the marker
         BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.fisher);
         Bitmap icon = Bitmap.createScaledBitmap(bitmapdraw.getBitmap(), 150, 150, false);
 
+        // Display the marker
         LatLng latLng = new LatLng(latitude, longitude);
         String id = googleMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker()).position(latLng).title("C'est vous !").icon(BitmapDescriptorFactory.fromBitmap(icon))).getId();
         markerMap.put(id, "C'est vous !");
     }
 
+    // Show a station on the map
     private void showStationMarker(Station station) {
+        // Set a specific icon for the marker
         BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.station);
         Bitmap icon = Bitmap.createScaledBitmap(bitmapdraw.getBitmap(), 150, 150, false);
 
+        // Display the marker
         LatLng latLng = new LatLng(station.getLatitude(), station.getLongitude());
         String id = googleMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker()).position(latLng).title(station.getCodeStation() + " - " + station.getLibelleCommune()).icon(BitmapDescriptorFactory.fromBitmap(icon))).getId();
         markerMap.put(id, station.getCodeStation());
@@ -128,36 +153,49 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
 
+        // Hide the actionBar
+        getSupportActionBar().hide();
+
+        // Save the context
+        this.context = MapActivity.this;
+
+        // Initialize the statics variables
         MapActivity.stations = new ArrayList<>();
         MapActivity.markerMap = new HashMap<>();
         MapActivity.loadDone = false;
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-        getSupportActionBar().hide();
+        // Bind the map fragment with the activity
         SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
         supportMapFragment.getMapAsync(this);
 
+        // Add an action on the imageButton click
         findViewById(R.id.currentLocationImageButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (view.getId() == R.id.currentLocationImageButton && googleMap != null && currentLocation != null) {
+                    // Relocate the view on the current user location
                     MapActivity.this.animateCamera(currentLocation);
                 }
             }
         });
 
+        // Bind the department research bar and add an action
         EditText etZip = findViewById(R.id.etZipCode);
-
         etZip.setOnTouchListener((v, event) -> {
+            // Hide the keyboard
             InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-            final int DRAWABLE_RIGHT = 2;
+
+            // Check if the drawable is clicked
             if (event.getAction() == MotionEvent.ACTION_UP) {
-                if (event.getRawX() >= (etZip.getRight() - etZip.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                if (event.getRawX() >= (etZip.getRight() - etZip.getCompoundDrawables()[2].getBounds().width())) {
                     if (MapActivity.stations != null) {
                         boolean founded = false;
+
+                        // Check if it exist a station for the entered zip code and show it if yes
                         for (Station station : MapActivity.stations) {
                             if (station.getCodeDepartement().equals(etZip.getText().toString())) {
                                 Location location = new Location("");
@@ -168,6 +206,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                                 break;
                             }
                         }
+                        // If there is no station, display a message
                         if (etZip.getText() != null && !founded) {
                             Toast.makeText(context, "Aucune station pour ce département !", Toast.LENGTH_SHORT).show();
                         }
@@ -177,31 +216,27 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             }
             return false;
         });
-
-        this.context = MapActivity.this;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
+
+        // Define a custom infoView to be displayed on a marker click
         CustomInfo customInfo = new CustomInfo(this);
         this.googleMap.setInfoWindowAdapter(customInfo);
-        this.googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                String markerId = markerMap.get(marker.getId());
-                if (!markerId.equals("C'est vous !")) {
-                    Station station = findStationByCode(markerId);
-                    // DO NOTHING
-                }
-                return false;
-            }
-        });
-        this.googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
-            @Override
-            public void onInfoWindowClick(Marker marker) {
-                String stationCode = markerMap.get(marker.getId());
-                Station station = findStationByCode(stationCode);
+
+        // Add an action on the click
+        this.googleMap.setOnInfoWindowClickListener(marker -> {
+            // Get the marker
+            String markerId = markerMap.get(marker.getId());
+
+            // If the marker is not the user marker
+            if (!markerId.equals("C'est vous !")) {
+                // Get the matching station
+                Station station = findStationByCode(markerId);
+
+                // If the station has been loaded display the station activity
                 if (station.isLoaded()) {
                     Intent intent = new Intent(MapActivity.this, StationActivity.class);
                     intent.putExtra("station", station);
@@ -209,23 +244,32 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 } else {
                     Toast.makeText(context, "La station n'a pas encore fini de charger", Toast.LENGTH_SHORT).show();
                 }
+            } else {
+                Toast.makeText(context, "Vous n'êtes pas une station !", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    /**
+     * Create a trigger on user location updated (each 10 minutes).
+     * Add a callback on location change
+     */
     private void startCurrentLocationUpdates() {
         LocationRequest locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         locationRequest.setInterval(600000);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MapActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            ActivityCompat.requestPermissions(MapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
             return;
         }
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, mLocationCallback, Looper.myLooper());
     }
 
+    /**
+     * Check if the google play service is available for google maps correct usage.
+     *
+     * @return true or false
+     */
     private boolean isGooglePlayServicesAvailable() {
         GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
         int status = googleApiAvailability.isGooglePlayServicesAvailable(this);
@@ -249,6 +293,11 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         }
     }
 
+    /**
+     * Make an animation with the camera going to a specified location.
+     *
+     * @param location
+     */
     private void animateCamera(@NonNull Location location) {
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(getCameraPositionWithBearing(latLng)));
@@ -282,6 +331,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         googleMap = null;
     }
 
+    /**
+     * Find a station by is code.
+     *
+     * @param code The station code
+     * @return The corresponding station
+     */
     private Station findStationByCode(String code) {
         Station result = null;
         for (Station station : MapActivity.stations) {
